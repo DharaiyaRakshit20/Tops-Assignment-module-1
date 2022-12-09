7ed1f569d5d5012c825114460b89f13e10126512
DharaiyaRakshit20 <115060570+DharaiyaRakshit20@users.noreply.github.com>
2022-12-09 04:09:37 -0800

    first change

---
+++ Module.3/3.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    
+    char name;
+    int bd;
+    int age;
+    char addrese;
+
+    printf("Enter your name;");
+    scanf("%c",&name);
+    printf("Your name:%c",name);
+
+    return 0;
+}
