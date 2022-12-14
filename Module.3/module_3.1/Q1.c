457f3f53b102d670ecffca305851a8d1931c8a13
DharaiyaRakshit20 <115060570+DharaiyaRakshit20@users.noreply.github.com>
2022-12-13 21:27:44 -0800

    fore change

---
+++ Module.3/module_3.1/Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char name,addrese;
+    int age,birthdate;
+    printf("Your name:Dharaiya Rakshit Rajeshbhai\n");
+    printf("Your age:20\n");
+    printf("Your Birthdate:11/06/2022\n");
+    printf("Your addrese:628,Satynarayan socity,punagam,surat-6\n");
+
+    return 0;
+}
---
+++ Module.3/module_3.1/Q6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+    int days,years;
+    days=543;
+    years=days/365;
+    printf("\n Number of years is%d",years);
+
+    return 0;
+}
